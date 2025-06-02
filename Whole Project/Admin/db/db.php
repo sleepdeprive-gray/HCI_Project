5306f592dfc39f427b705c82b2b3a75c6eeb526d
Hermoso1172 <ic.clarenceherold.hermoso@cvsu.edu.ph>
2025-06-02 22:01:37 +0800

    changing the name

--- Whole Project/Admin/db/db.php
+++ Whole Project/Admin/db/db.php
@@ -1,3 +1,13 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'bk_database');
+   
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "bk_database";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
