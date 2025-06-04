badad4b9c9bedb699c52dbec10d4fff7bc8ef713
Hermoso1172 <ic.clarenceherold.hermoso@cvsu.edu.ph>
2025-06-04 23:44:43 +0800

    delete the old

--- Member/member_function/removeSAVEDbooks.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if(isset($_POST['remove'])){
-        $saveID = $_POST['nums'];
-        $deleteSAVEbooks = mysqli_query($conn,"DELETE FROM save_book WHERE savedBookID = $saveID");
-        
-    }
-?>
